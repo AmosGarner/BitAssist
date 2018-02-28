7265e5650d7be7f1700be9f42a5536bd918f9bc0
amosgarner <aagarner.7@gmail.com>
2018-02-28 15:27:36 -0500

    Refactor styles layout and implement call to geolocation to get lat and long.

---
+++ UI/Assets/Styles.js
@@ -0,0 +1,11 @@
+export default {
+    "appContainer" : {
+        "ios" : {
+            "flex": 1,
+            "justifyContent": 'center',
+            "alignItems": 'center',
+            "backgroundColor": '#46413c',
+
+        }
+    }
+}
--- UI/Containers/LocationBlock.js
+++ UI/Containers/LocationBlock.js
@@ -7,12 +7,41 @@ import {
 } from 'react-native';
 
 export default class LocationBlock extends Component<{},{}>{
+    constructor(){
+        super();
+        this.state = {
+            latitude: null,
+            longitude: null,
+            city: null,
+
+        }
+    }
+
+    componentWillMount(){
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude
+                });
+            },
+            (error) => {
+                alert(error.message);
+            },
+            {
+                enableHighAccuracy: true,
+                timeout: 20000,
+                maximumAge: 1000
+            }
+        );
+    }
+
     render(){
         return(
             <View style={this.props.style}>
                 <Text>Location: </Text>
                 <Text>{this.props.location.city} ,{this.props.location.state}</Text>
-                <Text>{this.props.location.latitude}&deg;N  : {this.props.location.longitude}&deg;S</Text>
+                <Text>{this.state.latitude} : {this.state.longitude}</Text>
             </View>
         );
     }
