c15b26396e3fea57062db586cc4a9eefc1a60b97
amosgarner <aagarner.7@gmail.com>
2018-03-09 14:17:15 -0500

    Implement refresh once per second.

--- App.js
+++ App.js
@@ -16,26 +16,40 @@ import BinTimeBlock from "./UI/Containers/BinTimeBlock";
 import WeatherBlock from "./UI/Containers/WeatherBlock";
 import LocationBlock from "./UI/Containers/LocationBlock";
 import Styles from "./UI/Assets/Styles";
+import TimerMixin from 'react-timer-mixin';
 
 export default class App extends Component<{},State> {
+    constructor(){
+        super();
+        this.state = {
+            timeObject : new TimeObject()
+        }
+    }
+
+    componentDidMount(){
+        this.interval = setInterval(() => {
+            this.setState({
+                timeObject: new TimeObject()
+            })
+        }, 1000);
+    }
 
     render() {
-    let timeObject = new TimeObject();
-    return (
-      <View style={[Styles.appContainer, Styles.ios]}>
-          <View style={Styles.header}>
-              <HeaderBlock/>
+        return (
+          <View style={[Styles.appContainer, Styles.ios]}>
+              <View style={Styles.header}>
+                  <HeaderBlock/>
+              </View>
+              <View style={Styles.content}>
+                  <TimeBlock style={[Styles.row]} time={this.state.timeObject}/>
+                  <BinTimeBlock style={[Styles.row]} time={this.state.timeObject}/>
+                  <HexTimeBlock style={[Styles.row]} time={this.state.timeObject}/>
+                  <DateBlock style={[Styles.row]} date={new DateObject()}/>
+                  <WeatherBlock style={[Styles.row]} />
+                  <LocationBlock style={[Styles.row]}/>
+              </View>
+              <View style={Styles.footer}/>
           </View>
-          <View style={Styles.content}>
-              <TimeBlock style={[Styles.row]} time={timeObject}/>
-              <BinTimeBlock style={[Styles.row]} time={timeObject}/>
-              <HexTimeBlock style={[Styles.row]} time={timeObject}/>
-              <DateBlock style={[Styles.row]} date={new DateObject()}/>
-              <WeatherBlock style={[Styles.row]} />
-              <LocationBlock style={[Styles.row]}/>
-          </View>
-          <View style={Styles.footer}/>
-      </View>
-    );
+        );
   }
 }
